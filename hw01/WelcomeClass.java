ea99fd8b02f894edcdabd055782319dcbe202292
hul214 <huahao90@gmail.com>
2014-09-02 22:29:09 +0000

    hw1 welcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////
+//Huahao Luo hul214
+//
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+//  then Test if the output is similar to the sample
+
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args){
+    
+//  print the statement with box around it
+    System.out.println("    -----------");
+    System.out.println("    | WELCOME |");
+    System.out.println("    -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    // As I search on the internet, in order to pring to "\" to the screen,
+    // I have to put two "\"s in the line like below.
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-h--u--l--2--1--4-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I have been away from Lehigh for 8 month,");
+    System.out.println("and I kind of miss my life here.");
+    System.out.println("Now I'm a Industrial Engineer X Street Photographer.");
+    
+    }
+}
+
+// don't for get compile before actually run the program.
